87798e045d805aed6c2651055d01fd7049231c46
moriya <mhikaru.job@gmail.com>
2024-09-21 14:41:07 +0900

    abc370b commit

--- 2024/abc370/b/main.py
+++ 2024/abc370/b/main.py
@@ -1,9 +1,11 @@
 N = int(input())
 A = [list(map(int, input().split())) for _ in range(N)]
 
-for i in range(N):
-  if 1 >= i:
-    tmp = A[0][i]
+tmp = 1
+for i in range(1, N+1):
+  if tmp >= i:
+    tmp = A[tmp-1][i-1]
   else:
-    tmp = A[i][0]
+    tmp = A[i-1][tmp-1]
 
+print(tmp)
--- 2024/abc370/c/main.py
+++ 2024/abc370/c/main.py
@@ -1,3 +1,19 @@
-N = int(input())
-A, B = map(int, input().split())
-L = list(map(int, input().split()))
+S = list(input())
+T = list(input())
+
+X = []
+# 先頭から見る
+for i in range(len(S)):
+  if S[i] > T[i]:
+    S[i] = T[i]
+    X.append(S[:])
+
+# 後ろから見る
+for i in reversed(range(len(S))):
+  if S[i] < T[i]:
+    S[i] = T[i]
+    X.append(S[:])
+
+print(len(X))
+for x in X:
+  print(''.join(x))
